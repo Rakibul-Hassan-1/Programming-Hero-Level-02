0091a1bfefd755414ea33104d955ca2a0f996727
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2025-08-18 21:15:55 +0600

    GET POST method learing

--- Be an Ecpress and Mongoose Masters/module-14 Todo App/server.js
+++ Be an Ecpress and Mongoose Masters/module-14 Todo App/server.js
@@ -1,9 +1,21 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Welcome to the Todo App");
+  // console.log(req.url, req.method);
+  // res.end("Welcome to the Todo App");
+  if (req.url === "/todos" && req.method === "GET") {
+    res.end("All Todos");
+  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    res.end("Create Todo");
+  } else {
+    res.end("404 Not Found");
+  
+  }
 });
 
 server.listen(5000, "127.0.0.1", () => {
   console.log("✅ Server is running on http://localhost:5000");
 });
+
+// /todos - GET - All Todos
+// /todos/create todo - POST - Create Todo
