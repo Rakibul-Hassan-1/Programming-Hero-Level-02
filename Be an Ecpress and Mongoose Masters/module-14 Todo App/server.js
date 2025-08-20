229a5d8443fc45b31d319ba00e6ad2c6b0b9d581
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2025-08-20 11:20:35 +0600

    customize meta data using writeHead function, also can modify status code 200

--- Be an Ecpress and Mongoose Masters/module-14 Todo App/server.js
+++ Be an Ecpress and Mongoose Masters/module-14 Todo App/server.js
@@ -4,12 +4,18 @@ const server = http.createServer((req, res) => {
   // console.log(req.url, req.method);
   // res.end("Welcome to the Todo App");
   if (req.url === "/todos" && req.method === "GET") {
+    // res.end("All Todos");
+
+    //  customize meta data using writeHead function, also can modify status code 200
+    res.writeHead(201, {
+      "content-type": "text/plain",
+      email: "test@email.com",
+    });
     res.end("All Todos");
   } else if (req.url === "/todos/create-todo" && req.method === "POST") {
     res.end("Create Todo");
   } else {
     res.end("404 Not Found");
-  
   }
 });
 
