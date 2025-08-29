abea4b2dd4b204c9dfb43cebc2c7c265c34601c9
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2025-08-29 11:30:21 +0600

    add shad cn into my React-Redux learing project

--- Be a Redux Reaper/Module 22 - Bootstrapping Redux Basic Project/redux-bootstrap/src/App.tsx
+++ Be a Redux Reaper/Module 22 - Bootstrapping Redux Basic Project/redux-bootstrap/src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button";
 import { decrement, increment } from "./redux/features/counter/counterSlice";
 import { useAppDispatch, useAppSelector } from "./redux/hook";
 function App() {
@@ -14,13 +15,43 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Counter With Redux</h1>
-        <button onClick={() => handleIncrement(5)}>Increment By 5</button>
-        <button onClick={() => handleIncrement(1)}>Increment</button>
-        <div>{count}</div>
-        <button onClick={() => handleDecrement(5)}>Decrement by 5</button>
-        <button onClick={() => handleDecrement(1)}>Decrement</button>
+      <div className="flex min-h-svh flex-col items-center justify-center space-y-8">
+        <h1 className="text-4xl font-bold">Counter With Redux</h1>
+        <div className="flex flex-col items-center space-y-4">
+          <div className="flex gap-4">
+            <Button
+              onClick={() => handleIncrement(5)}
+              variant="secondary"
+              className="bg-blue-500 hover:bg-blue-600 text-white"
+            >
+              Increment By 5
+            </Button>
+            <Button
+              onClick={() => handleIncrement(1)}
+              className="bg-blue-500 hover:bg-blue-600"
+            >
+              Increment
+            </Button>
+          </div>
+          <div className="text-4xl font-semibold bg-slate-100 px-8 py-4 rounded-lg">
+            {count}
+          </div>
+          <div className="flex gap-4">
+            <Button
+              onClick={() => handleDecrement(1)}
+              className="bg-red-500 hover:bg-red-600"
+            >
+              Decrement
+            </Button>
+            <Button
+              onClick={() => handleDecrement(5)}
+              variant="secondary"
+              className="bg-red-500 hover:bg-red-600 text-white"
+            >
+              Decrement by 5
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   );
